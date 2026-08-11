6a07703e27e7a1a7a8ba222f2e90edd0b1f92cd6
pranesh magar <praneshmagar05@gmail.com>
2026-08-11 14:02:40 -0400

    Practice loops and string methods

---
+++ for-loops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    /*for(int i = 5; i >= 1; i--){ // first condition i = index which is a counter... second condition is a stopping condition... third condition is increment 
+        std::cout << i << '\n';
+    }
+        std::cout << "Happy New Years!\n";
+    return 0;*/
+
+    for(int i = 3; i>=1; i--){
+        std::cout<< i << '\n';
+    }
+        std::cout << "YESSSSSSS! " <<'\n';
+    return 0;
+}
