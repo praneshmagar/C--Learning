3a019fdb63a7e317f4787754ecda89a37c19adfd
pranesh magar <praneshmagar05@gmail.com>
2026-08-11 14:42:28 -0400

    Practice loops and string methods

---
+++ Random-event-generator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+
+int main()
+{
+    srand(time(0));
+    int randNum = rand() % 5 + 1;
+
+    switch(randNum){
+        case 1: std::cout << "You win a Bumper sticker!\n";
+                break;
+        case 2:std::cout << "You win a cheese steak!\n";
+                break;
+        case 3:std::cout << "You win a car!\n";
+                break;
+        case 4:std::cout << "You win a shirt!\n";
+                break;
+        case 5:std::cout << "You win a phone!\n";
+                break;
+    }
+
+
+
+    return 0;
+}
